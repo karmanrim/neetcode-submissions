7d1b14c6d64dfd73741abb64759386a0726f4c07
Roman <153538502+karmanrim@users.noreply.github.com>
2026-05-07 14:45:26 +0300

    Add: combination-target-sum - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        def bruteforce(nums, results, result, target, cur_sum):
+            if cur_sum > target or len(nums) == 0:
+                return 
+            if cur_sum == target:
+                results.add(tuple(result.copy()))
+                return 
+            result.append(nums[0])
+            cur_sum += nums[0]
+            bruteforce(nums, results, result, target, cur_sum)
+            cur_sum -= nums[0]
+            result.pop()
+            bruteforce(nums[1:], results, result, target, cur_sum)
+            return
+
+        results = set()
+        bruteforce(nums, results, [], target, 0)
+        return [list(result) for result in results ]
+
